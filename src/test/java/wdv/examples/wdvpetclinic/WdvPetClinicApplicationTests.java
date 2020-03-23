0d83b3bb321b63e971f0fcb5f6a29679e3b1f5b7
Walter <wd.vergara@paltolab.com>
2020-03-23 09:34:24 +0100

    Initial Project Add

---
+++ src/test/java/wdv/examples/wdvpetclinic/WdvPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package wdv.examples.wdvpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdvPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
