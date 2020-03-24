38b1be65c1976ea31804da467581970a5016785e
walterwootz <wd.vergara@paltolab.com>
2020-03-24 16:23:18 +0100

    Implement BaseEntity.

--- pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package wdv.examples.wdvpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package wdv.examples.wdvpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
