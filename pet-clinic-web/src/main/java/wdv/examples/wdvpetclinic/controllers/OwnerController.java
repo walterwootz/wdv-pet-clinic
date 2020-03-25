5ea949905a0ed1d42583dad82589ab90c2dad9ad
walterwootz <wd.vergara@paltolab.com>
2020-03-25 17:18:15 +0100

    List all Owners on Owner Index Page. Closes #18

--- pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package wdv.examples.wdvpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wdv.examples.wdvpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"", "/index", "/index.html"})
-    public String showOwners(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String showOwnersAll(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
