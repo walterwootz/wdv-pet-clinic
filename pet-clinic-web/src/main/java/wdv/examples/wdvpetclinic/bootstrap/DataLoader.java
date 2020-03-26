4c31700bf72473a50dd424172787298008e5dc2c
walterwootz <wd.vergara@paltolab.com>
2020-03-26 17:49:09 +0100

    Add PetTypes in with Bootstrap (startup). Closes #39

--- pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package wdv.examples.wdvpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import wdv.examples.wdvpetclinic.models.Owner;
+import wdv.examples.wdvpetclinic.models.PetType;
 import wdv.examples.wdvpetclinic.models.Vet;
 import wdv.examples.wdvpetclinic.services.OwnerService;
+import wdv.examples.wdvpetclinic.services.PetTypeService;
 import wdv.examples.wdvpetclinic.services.VetService;
 
 @Component
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
