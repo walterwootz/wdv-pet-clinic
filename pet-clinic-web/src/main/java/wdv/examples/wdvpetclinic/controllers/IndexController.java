bc036e9ae8c0cb9a5f413916d8e47c68f7cc0533
walterwootz <wd.vergara@paltolab.com>
2020-03-26 16:13:45 +0100

    Add missing i18n properties file. Closes #32

--- pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/IndexController.java
@@ -1,15 +1,13 @@
 package wdv.examples.wdvpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"/", "", "index", "index.html"})
-    public String index(Model model){
-        model.addAttribute("welcome", "Welcome to WDV Pet Clinic!");
+    public String index(){
         return "index";
     }
 }
