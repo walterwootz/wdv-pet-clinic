8ab5aa7bc4cbb8ef56c532d2fd9dca11009df92e
walterwootz <wd.vergara@paltolab.com>
2020-03-26 16:03:20 +0100

    Apply master layout to Index Page. Closes #21

--- pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/wdv/examples/wdvpetclinic/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package wdv.examples.wdvpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"/", "", "index", "index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("welcome", "Welcome to WDV Pet Clinic!");
         return "index";
     }
 }
