0e7cf70aa925f00a3f39ce8fb23f722a11371a2a
walterwootz <wd.vergara@paltolab.com>
2020-03-26 17:42:39 +0100

    Added PetType Service Interface and PetType Service Map Implementation. Closes #36

---
+++ pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package wdv.examples.wdvpetclinic.services;
+
+import wdv.examples.wdvpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/wdv/examples/wdvpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package wdv.examples.wdvpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import wdv.examples.wdvpetclinic.models.PetType;
+import wdv.examples.wdvpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
